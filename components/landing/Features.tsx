567876dbb7c26747eb2cb5566774957c494fb39e
Mario <mabel010830@gmail.com>
2026-02-19 13:48:54 -0500

    Fixed error in type of Easing

--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -67,7 +67,7 @@ const cardVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.5, ease: [0.25, 0.1, 0.25, 1] },
+    transition: { duration: 0.5, ease: [0.25, 0.1, 0.25, 1] as const },
   },
 };
 
